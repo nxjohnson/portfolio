e6874ed0e9118979a03fec584cd86c952c6c8149
Neil <neiljohnson92@gmail.com>
2022-08-26 17:19:31 -0700

    Added new about me and created css color scheme

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -25,7 +25,16 @@ const AboutMe = () => {
         <div className={styles.aboutTile}>
           <h2 className={styles.title}>About Me</h2>
           <p>
-          Previously, I was a Professional Wedding and Event Photographer for over ten years working at a prestigious photography studio. I loved being able to create and problem solve every day while telling my clients stories. In 2020, I began my entrepreneurial journey, and one of my favorite tasks was building my e-commerce website. At the time, I was using Shopify, and once my shop matured, one obstacle I faced was going beyond what my website theme provided, which led me down the path of learning to code and igniting a new passion. At the beginning of 2022, I enrolled in Hack Reactor&apos;s Advance Software Engineering Immersive, where I worked with other software engineers in various sprints building full-stack applications in an Agile environment using JavaScript ES6, React, Node.js, Postgres, and AWS EC2 Instances.
+            Prior to exploring the Software Engineering industry, I was a Professional Wedding and Event Photographer for over ten years, working at a prestigious photography studio. I loved being able to create and problem-solve while telling my clients stories.
+          </p>
+          <p>
+            In 2020, I began my entrepreneurial journey, and one of my favorite tasks was building my e-commerce website. At the time, I was using Shopify, and once my shop matured, one obstacle I faced was going beyond what my website theme provided, which led me down the path of learning to code and igniting a new passion.
+          </p>
+          <p>
+            At the beginning of 2022, I enrolled in Hack Reactor&apos;s Advance Software Engineering Immersive, where I learned how to build full-stack applications in an Agile environment using JavaScript, React, Next.js, Node.js, and SQL and NoSQL Databases.
+          </p>
+          <p>
+            While my background isn&apos;t traditional, I have a unique and deep skill set complimented with my strong passion for learning, creating, and problem solving. If you are interested in building new web experiences together, let&apos;s chat.
           </p>
         </div>
       </div>
